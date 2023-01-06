3d9d9b59f16be9f1aea01bd7f9660e1b242aba61
amywu23 <111457341+amywu23@users.noreply.github.com>
2023-01-06 19:01:42 +0000

    finalProject

--- TicTacToe.java
+++ TicTacToe.java
@@ -25,7 +25,7 @@ public class TicTacToe{
         
         board = new Board();
         board.drawBoard();
-for(int x =0; x <4; x++){
+while(!isGameOver()){
         if (playTurn == 0){
             Scanner s = new Scanner(System.in);
             System.out.println("Player 1, please enter a position to be replaced by X:");
@@ -37,6 +37,12 @@ for(int x =0; x <4; x++){
             Position.set(one-1, player1);}
             playTurn++; 
             callArray();
+            if(isGameOver()){
+                // System.out.println("player 1 wins");
+                break;
+  
+            }
+
         }    
        
         if (playTurn == 1){
@@ -50,18 +56,23 @@ for(int x =0; x <4; x++){
         Position.set(one-1, player2);}
         playTurn--;
         callArray();
+        if(isGameOver()){
+            // System.out.println("player 2 wins");
+            break;
+
+        }
         }
     }
-    Scanner s = new Scanner(System.in);
-            System.out.println("Player 1, please enter a position to be replaced by X:");
-            int one = s.nextInt();
-            if(one%3==0){
-                Position.set(one-1, "X\n");
-            }
-            else{
-            Position.set(one-1, player1);}
-            playTurn++; 
-            callArray();
+    // Scanner s = new Scanner(System.in);
+    //         System.out.println("Player 1, please enter a position to be replaced by X:");
+    //         int one = s.nextInt();
+    //         if(one%3==0){
+    //             Position.set(one-1, "X\n");
+    //         }
+    //         else{
+    //         Position.set(one-1, player1);}
+    //         playTurn++; 
+    //         callArray();
     
     }
 
@@ -90,5 +101,95 @@ for(int x =0; x <4; x++){
         }
 
     }
+
+public boolean isGameOver(){
+if (Position.get(0).equals("X") && Position.get(1).equals("X") && Position.get(2).equals("X\n")){
+ System.out.println("1 wins");
+    return true;
+}
+if (Position.get(3).equals("X") && Position.get(4).equals("X") && Position.get(5).equals("X\n")){
+    System.out.println("1 wins");
+       return true;
+}
+if (Position.get(6).equals("X") && Position.get(8).equals("X") && Position.get(8).equals("X\n")){
+    System.out.println("1 wins");
+       return true;
+}
+
+//diagonal
+
+
+if (Position.get(0).equals("X") && Position.get(4).equals("X") && Position.get(8).equals("X\n")){
+    System.out.println("1 wins");
+       return true;
+}
+if (Position.get(2).equals("X\n") && Position.get(4).equals("X") && Position.get(6).equals("X")){
+    System.out.println("1 wins");
+       return true;
+}
+
+//vertical
+
+if (Position.get(0).equals("X") && Position.get(3).equals("X") && Position.get(6).equals("X")){
+    System.out.println("1 wins");
+       return true;
+}
+if (Position.get(1).equals("X") && Position.get(4).equals("X") && Position.get(7).equals("X")){
+    System.out.println("1 wins");
+       return true;
+}
+if (Position.get(2).equals("X\n") && Position.get(5).equals("X\n") && Position.get(8).equals("X\n")){
+    System.out.println("1 wins");
+       return true;
+}
+
+
+
+
+
+if (Position.get(0).equals("O") && Position.get(1).equals("O") && Position.get(2).equals("O\n")){
+    System.out.println("2 wins");
+       return true;
+   }
+   if (Position.get(3).equals("O") && Position.get(4).equals("O") && Position.get(5).equals("O\n")){
+       System.out.println("2 wins");
+          return true;
+   }
+   if (Position.get(6).equals("O") && Position.get(8).equals("O") && Position.get(8).equals("O\n")){
+       System.out.println("2 wins");
+          return true;
+   }
+   
+   //diagonal
+   
+   
+   if (Position.get(0).equals("O") && Position.get(4).equals("O") && Position.get(8).equals("O\n")){
+       System.out.println("2 wins");
+          return true;
+   }
+   if (Position.get(2).equals("O\n") && Position.get(4).equals("O") && Position.get(6).equals("O")){
+       System.out.println("2 wins");
+          return true;
+   }
+   
+   //vertical
+   
+   if (Position.get(0).equals("O") && Position.get(3).equals("O") && Position.get(6).equals("O")){
+       System.out.println("2 wins");
+          return true;
+   }
+   if (Position.get(1).equals("O") && Position.get(4).equals("O") && Position.get(7).equals("O")){
+       System.out.println("2 wins");
+          return true;
+   }
+   if (Position.get(2).equals("O\n") && Position.get(5).equals("O\n") && Position.get(8).equals("O\n")){
+       System.out.println("2 wins");
+          return true;
+   }
+   
+return false;
+}
+
+
 
 }
